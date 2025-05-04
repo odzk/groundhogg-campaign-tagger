138e114027fa899213046e856e51d8521e53434e
odzk <ody@web-mech.net>
2025-05-04 13:51:32 +0800

    fix tagging campaign

--- groundhogg-campaign-tagger.php
+++ groundhogg-campaign-tagger.php
@@ -1,10 +1,13 @@
 <?php
-/**
- * Plugin Name: Groundhogg Campaign Tagger
- * Description: Automatically tags Mailgun emails with the campaign name from Groundhogg Broadcasts.
- * Version: 1.1
- * Author: Odysseus Ambut
- */
+/*
+Plugin Name: Groundhogg Campaign Tagger
+Description: Automatically tags Mailgun emails with the campaign name from Groundhogg Broadcasts.
+Version: 1.2
+Author: Odysseus Ambut
+Author URI: https://web-mech.net
+GitHub Plugin URI: https://github.com/odzk/groundhogg-campaign-tagger
+Primary Branch: main
+*/
 
 use Groundhogg\Plugin;
 use Groundhogg\Broadcast;
@@ -38,12 +41,28 @@ add_filter('groundhogg/send_email/wp_mail_args', function ($args, $email, $conta
 // Use the builder hook to add multiple tags
 add_filter('groundhogg/mailgun/send/api/builder', function($builder, $params, $headers, $to) {
 
-    if (isset($headers['X-GH-Campaign-Name'])) {
-        $builder->addTag(sanitize_text_field($headers['X-GH-Campaign-Name']));
+    // Try to extract campaign name from the email object via broadcast meta
+    if (!empty($params['email_id'])) {
+        $email = new \Groundhogg\Email($params['email_id']);
+        $broadcast_id = absint($email->get_meta('_broadcast_id'));
+
+        if ($broadcast_id) {
+            $broadcast = new \Groundhogg\Broadcast($broadcast_id);
+            $campaign_id = $broadcast->get_campaign_id();
+
+            if ($campaign_id) {
+                $campaign = \Groundhogg\Plugin::instance()->dbs->get_db('campaigns')->get($campaign_id);
+
+                if ($campaign && !empty($campaign->name)) {
+                    $builder->addTag(sanitize_text_field($campaign->name));
+                }
+            }
+        }
     }
 
-    // Always tag with "groundhogg" as well
-    $builder->addTag('groundhogg');
+    // Always add fallback/default tag
+    $builder->addTag('Campaign Test');
 
     return $builder;
+
 }, 10, 4);
