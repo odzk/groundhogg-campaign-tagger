800e99c29d531d8126f0e1762f5a5d3453ae642d
odzk <ody@web-mech.net>
2025-05-04 23:17:01 +0800

    Update groundhogg-campaign-tagger.php

--- groundhogg-campaign-tagger.php
+++ groundhogg-campaign-tagger.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Groundhogg Campaign Tagger
 Description: Automatically tags Mailgun emails with the campaign name from Groundhogg Broadcasts.
-Version: 1.2.3
+Version: 1.2.4
 Author: Odysseus Ambut
 Author URI: https://web-mech.net
 GitHub Plugin URI: https://github.com/odzk/groundhogg-campaign-tagger
@@ -20,19 +20,30 @@ add_filter('groundhogg/send_email/wp_mail_args', function ($args, $email, $conta
 
     // Only continue if it's a broadcast
     $broadcast_id = absint($email->get_meta('_broadcast_id'));
-    if (!$broadcast_id) return $args;
+    if (!$broadcast_id) {
+        error_log('[GH Campaign Tagger] No broadcast ID found for email.');
+        return $args;
+    }
 
     $broadcast = new Broadcast($broadcast_id);
     $campaign_id = $broadcast->get_campaign_id();
 
-    if (!$campaign_id) return $args;
+    if (!$campaign_id) {
+        error_log('[GH Campaign Tagger] No campaign ID found for broadcast ID: ' . $broadcast_id);
+        return $args;
+    }
 
     $campaign = Plugin::instance()->dbs->get_db('campaigns')->get($campaign_id);
 
-    if (!$campaign || empty($campaign->name)) return $args;
+    if (!$campaign || empty($campaign->name)) {
+        error_log('[GH Campaign Tagger] No campaign or campaign name found for campaign ID: ' . $campaign_id);
+        return $args;
+    }
 
     // Add campaign name to headers
-    $args['headers'][] = 'X-GH-Campaign-Name: ' . sanitize_text_field($campaign->name);
+    $campaign_name = sanitize_text_field($campaign->name);
+    $args['headers'][] = 'X-GH-Campaign-Name: ' . $campaign_name;
+    error_log('[GH Campaign Tagger] Added header X-GH-Campaign-Name: ' . $campaign_name);
 
     return $args;
 
@@ -52,18 +63,26 @@ add_filter('groundhogg/mailgun/send/api/builder', function ($builder, $params, $
         foreach ($headers as $header) {
             if (stripos($header, 'X-GH-Campaign-Name:') === 0) {
                 $campaign_name = trim(str_ireplace('X-GH-Campaign-Name:', '', $header));
+                error_log('[GH Mailgun Builder] Found campaign name in headers: ' . $campaign_name);
                 break;
             }
         }
+    } else {
+        error_log('[GH Mailgun Builder] Headers is not an array: ' . print_r($headers, true));
     }
 
     // Add campaign name as tag if found
     if ($campaign_name) {
-        $builder->addTag(sanitize_text_field($campaign_name));
+        $sanitized_campaign_name = sanitize_text_field($campaign_name);
+        $builder->addTag($sanitized_campaign_name);
+        error_log('[GH Mailgun Builder] Added tag: ' . $sanitized_campaign_name);
+    } else {
+        error_log('[GH Mailgun Builder] No campaign name found in headers.');
     }
 
     // Always tag with "groundhogg"
     $builder->addTag('groundhogg');
+    error_log('[GH Mailgun Builder] Added default tag: groundhogg');
 
     return $builder;
 
